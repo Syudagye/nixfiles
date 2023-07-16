d80918c1a438a699b548cb71907f27cfd74cfc4f
Syudagye <syudagye@gmail.com>
2023-07-16 18:29:49 +0200

    Added sccache

--- home/default.nix
+++ home/default.nix
@@ -11,12 +11,17 @@
           url = "https://gist.githubusercontent.com/Bondrake/27555c9d02c2882fd5e32f8ab3ed620b/raw/e1e5dd68761a7f7e6a253fcb64905466104b9df9/trim-generations.sh";
           sha256 = "0kkcxazaay71zdf7jqrsh6fpyzyji361fcfzcpdd4dmf68nj8xq3";
         }).outPath))
+        sccache
       ];
 
       sessionPath = [
         "$HOME/.cargo/bin"
         "$HOME/.local/bin"
       ];
+
+      sessionVariables = {
+        RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
+      };
 
       stateVersion = "22.05";
     };
