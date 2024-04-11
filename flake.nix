f348445d4cf5e46e4e846fc66c7e44cd874926e2
Syudagye <syudagye@gmail.com>
2024-04-11 23:25:21 +0200

    Updated lefthk

--- flake.nix
+++ flake.nix
@@ -7,9 +7,8 @@
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # leftwm.url = github:Syudagye/leftwm/x11rb;
     leftwm.url = github:leftwm/leftwm;
-    lefthk.url = github:leftwm/lefthk;
+    lefthk.url = github:Syudagye/lefthk;
     eww.url = github:elkowar/eww;
     hyprland.url = github:hyprwm/hyprland;
     funky-tags.url = github:Syudagye/funky-tags;
